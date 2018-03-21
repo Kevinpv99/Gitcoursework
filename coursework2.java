98661cb316e8c1261415d9a5a139814cae25bc8e
Kevinpv99 <37632522+Kevinpv99@users.noreply.github.com>
2018-03-21 17:40:43 +0000

    I have added functionality to buttons and have done the validation

--- coursework2.java
+++ coursework2.java
@@ -33,3 +33,132 @@ public class Coursework implements ActionListener   // creating the class and im
 
   		z.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exits the grid.
 	}
+
+public void actionPerformed(ActionEvent e)
+   {
+	ImageIcon empty = new ImageIcon("bart0.jpg"); // stores an empty image 
+        Icon tiles = new ImageIcon(); // 
+
+	if(e.getSource() == button[0])
+	{
+		nextposition = 0;	
+		if(position == 1|| position == 4){
+			tiles = button[0].getIcon();
+			button[0].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 0;
+		}
+	}
+	else if(e.getSource() == button[1])
+	{
+		nextposition = 1;
+		if(position == 0|| position == 2|| position == 5){
+			tiles = button[1].getIcon();
+			button[1].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 1;
+		}
+	}
+	else if(e.getSource() == button[2])
+	{	
+		nextposition = 2;
+		if(position == 1|| position == 3|| position == 6){
+			tiles = button[2].getIcon();
+			button[2].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 2;
+		}
+	}
+	else if(e.getSource() == button[3])
+	{	
+		nextposition = 3;
+		if(position == 2|| position == 7){	
+			tiles = button[3].getIcon();
+			button[3].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 3;	
+		}
+	}
+	else if(e.getSource() == button[4])
+	{	
+		nextposition = 4;
+		if(position == 0|| position == 5|| position == 8){	
+			tiles = button[4].getIcon();
+			button[4].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 4;
+		}
+	}
+	else if(e.getSource() == button[5])
+	{	
+		nextposition = 5;
+		if(position == 1|| position == 4|| position == 6|| position == 9){	
+			tiles = button[5].getIcon();
+			button[5].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 5;
+		}
+	}
+	else if(e.getSource() == button[6])
+	{	
+		nextposition = 6;	
+		if(position == 2|| position == 5|| position == 7|| position == 10){
+			tiles = button[6].getIcon();
+			button[6].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 6;		
+		}
+	}
+	else if(e.getSource() == button[7])
+	{	
+		nextposition = 7;
+		if(position == 3 || position == 6|| position == 11){	
+			tiles = button[7].getIcon();
+			button[7].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 7;
+		}
+	}
+	else if(e.getSource() == button[8])
+	{	
+		nextposition = 8;
+		if(position == 9|| position == 4){
+			tiles = button[8].getIcon();
+			button[8].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 8;
+		}
+	}
+	else if(e.getSource() == button[9])
+	{	
+		nextposition = 9;
+		if(position == 10|| position == 8|| position == 5){
+			tiles = button[9].getIcon();
+			button[9].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 9;
+		}
+	}
+	else if(e.getSource() == button[10])
+	{	
+		nextposition = 10;
+		if(position == 11|| position == 9|| position == 6){	
+			tiles = button[10].getIcon();
+			button[10].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 10;
+		}
+	}
+	else if(e.getSource() == button[11])
+	{	
+		nextposition = 11;
+		if(position == 7|| position == 10){	
+			tiles = button[11].getIcon();
+			button[11].setIcon(empty);
+			button[position].setIcon(tiles);
+			position = 11;
+		}
+      }
+    }
+}
+
